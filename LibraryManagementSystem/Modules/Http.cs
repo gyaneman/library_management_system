a704ae5399c18f4d0543be825ba135b8403a3c18
gyane <taka4456950@gmail.com>
2015-11-21 03:33:58 +0900

    楽天apiで取得する本のデータを定義

--- LibraryManagementSystem/Modules/Http.cs
+++ LibraryManagementSystem/Modules/Http.cs
@@ -11,9 +11,42 @@ using Newtonsoft.Json;
 
 namespace LibraryManagementSystem.Modules
 {
-    
-    class RakutenResponseBook
+    public class RakutenResponseBook
     {
+        public class RakutenResponseBookItem
+        {
+            public string title { get; set; }
+            public string titleKana { get; set; }
+            public string subTitle { get; set; }
+            public string subTitleKana { get; set; }
+            public string seriesName { get; set; }
+            public string seriesNameKana { get; set; }
+            public string contents { get; set; }
+            public string contentsKana { get; set; }
+            public string author { get; set; }
+            public string authorKana { get; set; }
+            public string publisherName { get; set; }
+            public string size { get; set; }
+            public string isbn { get; set; }
+            public string itemCaption { get; set; }
+            public string salesDate { get; set; }
+            public int    itemPrice { get; set; }
+            public int    listPrice { get; set; }
+            public int    discountRate { get; set; }
+            public int    discountPrice { get; set; }
+            public string itemUrl { get; set; }
+            public string affiliateUrl { get; set; }
+            public string smallImageUrl { get; set; }
+            public string mediumImageUrl { get; set; }
+            public string largeImageUrl { get; set; }
+            public string chirayomiUrl { get; set; }
+            public string availability { get; set; }
+            public int    postageFlag { get; set; }
+            public int    limitedFlag { get; set; }
+            public int    reviewCount { get; set; }
+            public string reviewAverage { get; set; }
+            public string booksGenreId { get; set; }
+        }
         public int count { get; set; }
         public int page { get; set; }
         public int first { get; set; }
@@ -21,7 +54,7 @@ namespace LibraryManagementSystem.Modules
         public int hits { get; set; }
         public int carrier { get; set; }
         public int pageCount { get; set; }
-
+        public List<RakutenResponseBookItem> Items { get; set; }
     }
     class Http
     {
